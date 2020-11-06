dfb7b4c56a51a05f1a08f801b4a6e942430532a6
Matthew Dodds <matthewpeterdodds@gmail.com>
2020-11-06 08:50:28 +0000

    Add Firebase

--- angular-pro-app-seed-master/src/app/app.module.ts
+++ angular-pro-app-seed-master/src/app/app.module.ts
@@ -1,3 +1,5 @@
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
@@ -30,3 +32,21 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
+
+/*
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyC9zjid4oFEQ0sTfN0NfbzTqvZOHQAwT5k",
+    authDomain: "angular-pro-fitness-app-6c425.firebaseapp.com",
+    databaseURL: "https://angular-pro-fitness-app-6c425.firebaseio.com",
+    projectId: "angular-pro-fitness-app-6c425",
+    storageBucket: "angular-pro-fitness-app-6c425.appspot.com",
+    messagingSenderId: "465190215690",
+    appId: "1:465190215690:web:9e01ba03966c09d450fff3",
+    measurementId: "G-NR0H9FXQ0K"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+ */
